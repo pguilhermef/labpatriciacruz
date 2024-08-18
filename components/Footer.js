2bcca8e147f4bc55140f21562e05e556991f2252
pguilhermef <pguilhermesantos@hotmail.com>
2024-08-17 22:25:09 -0300

    Ajustes na largura do footer

--- components/Footer.js
+++ components/Footer.js
@@ -10,61 +10,63 @@ import clock from "@/public/clock.svg"
 
 function Footer() {
   return (
-    <footer className="bg-paty-cyan bg-opacity-20 sm:h-44 flex flex-col sm:flex-row  items-center justify-center text-black">
-      {/* Redes - OK */}
-      <div className="sm:w-1/3 w-full h-full flex flex-col justify-evenly p-4 gap-y-2">
-        <div className="text-lg lg:text-xl font-medium">
-          Mais de 16 anos oferecendo qualidade, confiança e rapidez.
-        </div>
-        <div className="flex gap-x-4">
-          {/* Whatsapp */}
-          <Link href="https://wa.me/5521970181833" target="_blank" passHref>
-            <div className="bg-paty-blue flex justify-center items-center shadow-md rounded-lg overflow-hidden w-14 h-14">
-              <Image src={whatsapp} alt="Whatsapp" className="h-10 fill-slate-400" />
-            </div>
-          </Link>
+    <footer className="bg-paty-cyan bg-opacity-20 flex justify-center items-center">
+      <div className="sm:h-44 sm:w-5/6 2xl:w-4/6  flex flex-col sm:flex-row  items-center justify-center text-black">
+        {/* Redes - OK */}
+        <div className="sm:w-1/3 w-full h-full flex flex-col justify-evenly p-4 gap-y-2">
+          <div className="text-lg lg:text-xl font-medium">
+            Mais de 16 anos oferecendo qualidade, confiança e rapidez.
+          </div>
+          <div className="flex gap-x-4">
+            {/* Whatsapp */}
+            <Link href="https://wa.me/5521970181833" target="_blank" passHref>
+              <div className="bg-paty-blue flex justify-center items-center shadow-md rounded-lg overflow-hidden w-14 h-14">
+                <Image src={whatsapp} alt="Whatsapp" className="h-10 fill-slate-400" />
+              </div>
+            </Link>
 
-          {/* Instagram */}
-          <Link href="https://www.instagram.com/@labpatriciacruz" target="_blank" passHref>
-            <div className="bg-paty-blue  flex justify-center items-center shadow-md rounded-lg overflow-hidden w-14 h-14">
-              <Image src={instagram} alt="Instagram" className="h-10" />
-            </div>
-          </Link>
-        </div>
-      </div>
-      {/* Contato - OK*/}
-      <div className="sm:w-1/3 w-full h-full py-2 px-4">
-        <div className="font-extrabold">
-          <div className="text-2xl">Contato</div>
-        </div>
-        <div className="mt-3 font-medium">
-          {/* Mail */}
-          <div className="flex items-center gap-x-2">
-            <Image src={mail} className="h-8 w-8 m-none"/>
-            <div className="flex">
-              labpatriciacruz@gmail.com
-            </div>
-          </div>
-          {/* Telephone */}
-          <div className="flex items-center gap-x-2">
-            <Image src={telephone} className="h-8 w-8 m-none"/>
-            <div className="flex">
-            (21) 99971-9031
-            </div>
-          </div>
-          {/* Clock */}
-          <div className="flex items-center gap-x-2">
-            <Image src={clock} className="h-8 w-8 m-none"/>
-            <div className="flex">
-            Seg à Sáb de 9:00 às 18:00
-            </div>
+            {/* Instagram */}
+            <Link href="https://www.instagram.com/@labpatriciacruz" target="_blank" passHref>
+              <div className="bg-paty-blue  flex justify-center items-center shadow-md rounded-lg overflow-hidden w-14 h-14">
+                <Image src={instagram} alt="Instagram" className="h-10" />
+              </div>
+            </Link>
           </div>
         </div>
-      </div>
-      {/* Logo Patrícia Cruz - OK */}
-      <div className="sm:w-1/3 w-full h-full p-4 sm:order-2">
-        <div className="h-full flex flex-col md:flex-row justify-center items-center font-serif text-5xl md:space-x-2">
-          <span className="text-paty-blue">Patrícia</span><span className=" text-paty-cyan">Cruz</span>
+        {/* Contato - OK*/}
+        <div className="sm:w-1/3 w-full h-full py-2 px-4">
+          <div className="font-extrabold">
+            <div className="text-2xl">Contato</div>
+          </div>
+          <div className="mt-3 font-medium">
+            {/* Mail */}
+            <div className="flex items-center gap-x-2">
+              <Image src={mail} className="h-8 w-8 m-none"/>
+              <div className="flex">
+                labpatriciacruz@gmail.com
+              </div>
+            </div>
+            {/* Telephone */}
+            <div className="flex items-center gap-x-2">
+              <Image src={telephone} className="h-8 w-8 m-none"/>
+              <div className="flex">
+              (21) 99971-9031
+              </div>
+            </div>
+            {/* Clock */}
+            <div className="flex items-center gap-x-2">
+              <Image src={clock} className="h-8 w-8 m-none"/>
+              <div className="flex">
+              Seg à Sáb de 9:00 às 18:00
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Logo Patrícia Cruz - OK */}
+        <div className="sm:w-1/3 w-full h-full p-4 sm:order-2">
+          <div className="h-full flex flex-col md:flex-row justify-start items-center font-serif text-5xl md:space-x-2">
+            <span className="text-paty-blue">Patrícia</span><span className=" text-paty-cyan">Cruz</span>
+          </div>
         </div>
       </div>
     </footer>
