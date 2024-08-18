a47263871a58274ef7e845c739d04b8d88a44f38
pguilhermef <pguilhermesantos@hotmail.com>
2024-08-17 22:14:22 -0300

    Adiciona informações de contato, logo e horario

--- components/Footer.js
+++ components/Footer.js
@@ -3,10 +3,14 @@ import Link from "next/link";
 
 import whatsapp from "@/public/whatsapp.svg"
 import instagram from "@/public/instagram.svg"
+import mail from "@/public/mail.svg"
+import telephone from "@/public/telephone.svg"
+import clock from "@/public/clock.svg"
+
 
 function Footer() {
   return (
-    <footer className="bg-cyan-500 sm:h-44 flex flex-col sm:flex-row  items-center justify-center text-black">
+    <footer className="bg-paty-cyan bg-opacity-20 sm:h-44 flex flex-col sm:flex-row  items-center justify-center text-black">
       {/* Redes - OK */}
       <div className="sm:w-1/3 w-full h-full flex flex-col justify-evenly p-4 gap-y-2">
         <div className="text-lg lg:text-xl font-medium">
@@ -15,40 +19,52 @@ function Footer() {
         <div className="flex gap-x-4">
           {/* Whatsapp */}
           <Link href="https://wa.me/5521970181833" target="_blank" passHref>
-            <div className="bg-indigo-950 flex justify-center items-center shadow-md rounded-lg overflow-hidden w-14 h-14">
+            <div className="bg-paty-blue flex justify-center items-center shadow-md rounded-lg overflow-hidden w-14 h-14">
               <Image src={whatsapp} alt="Whatsapp" className="h-10 fill-slate-400" />
             </div>
           </Link>
 
           {/* Instagram */}
           <Link href="https://www.instagram.com/@labpatriciacruz" target="_blank" passHref>
-            <div className="bg-white flex justify-center items-center shadow-md rounded-lg overflow-hidden w-14 h-14">
+            <div className="bg-paty-blue  flex justify-center items-center shadow-md rounded-lg overflow-hidden w-14 h-14">
               <Image src={instagram} alt="Instagram" className="h-10" />
             </div>
           </Link>
         </div>
       </div>
-      {/* Contato */}
-      <div className="sm:w-1/3 w-full h-full p-1 bg-slate-600">
-        <div>
+      {/* Contato - OK*/}
+      <div className="sm:w-1/3 w-full h-full py-2 px-4">
+        <div className="font-extrabold">
           <div className="text-2xl">Contato</div>
         </div>
-        <div>
-          <div>
-            teste@gmail.com
+        <div className="mt-3 font-medium">
+          {/* Mail */}
+          <div className="flex items-center gap-x-2">
+            <Image src={mail} className="h-8 w-8 m-none"/>
+            <div className="flex">
+              labpatriciacruz@gmail.com
+            </div>
           </div>
-          <div>
+          {/* Telephone */}
+          <div className="flex items-center gap-x-2">
+            <Image src={telephone} className="h-8 w-8 m-none"/>
+            <div className="flex">
             (21) 99971-9031
+            </div>
           </div>
-          <div>
+          {/* Clock */}
+          <div className="flex items-center gap-x-2">
+            <Image src={clock} className="h-8 w-8 m-none"/>
+            <div className="flex">
             Seg à Sáb de 9:00 às 18:00
+            </div>
           </div>
         </div>
       </div>
-      {/* Logo Patrícia Cruz */}
-      <div className="sm:w-1/3 w-full h-full p-1 bg-slate-500 sm:order-2">
+      {/* Logo Patrícia Cruz - OK */}
+      <div className="sm:w-1/3 w-full h-full p-4 sm:order-2">
         <div className="h-full flex flex-col md:flex-row justify-center items-center font-serif text-5xl md:space-x-2">
-          <span>Patrícia</span><span>Cruz</span>
+          <span className="text-paty-blue">Patrícia</span><span className=" text-paty-cyan">Cruz</span>
         </div>
       </div>
     </footer>
