47503e955fff8daf5a4e43272cc6e6c5d5b3b957
pguilhermef <pguilhermesantos@hotmail.com>
2024-08-20 22:00:37 -0300

    Ajuste na disposição dos links do Footer

--- components/Footer.js
+++ components/Footer.js
@@ -11,23 +11,23 @@ import clock from "@/public/clock.svg"
 function Footer() {
   return (
     <footer className="flex bg-paty-cyan-opacity justify-center items-center">
-      <div className="sm:h-44 sm:w-5/6 2xl:w-4/6 flex flex-col sm:flex-row items-center justify-center text-black">
+      <div className="sm:h-46 sm:w-5/6 2xl:w-4/6 flex flex-col sm:flex-row items-center justify-center text-black">
         {/* Redes - OK */}
         <div className="sm:w-1/3 w-full h-full flex flex-col justify-evenly p-4 gap-y-2">
           <div className="text-lg lg:text-xl font-medium">
             Mais de 16 anos oferecendo qualidade, confiança e rapidez.
           </div>
-          <div className="flex gap-x-4">
+          <div className="flex gap-x-4 h-12">
             {/* Whatsapp */}
-            <Link href="https://wa.me/5521970181833" target="_blank" passHref>
-              <div className="bg-paty-blue flex justify-center items-center shadow-md rounded-lg overflow-hidden w-14 h-14">
+            <Link href="https://wa.me/5521999719031" target="_blank" passHref>
+              <div className="bg-paty-blue flex justify-center items-center shadow-md rounded-lg overflow-hidden w-12 h-12">
                 <Image src={whatsapp} alt="Whatsapp" className="h-10 fill-slate-400" />
               </div>
             </Link>
 
             {/* Instagram */}
             <Link href="https://www.instagram.com/@labpatriciacruz" target="_blank" passHref>
-              <div className="bg-paty-blue  flex justify-center items-center shadow-md rounded-lg overflow-hidden w-14 h-14">
+              <div className="bg-paty-blue  flex justify-center items-center shadow-md rounded-lg overflow-hidden w-12 h-12">
                 <Image src={instagram} alt="Instagram" className="h-10" />
               </div>
             </Link>
